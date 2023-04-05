7e28c487c7b654d8bf96818a889a6673de042fea
Andrew <93886645+AndrewTanga@users.noreply.github.com>
2023-04-05 17:07:54 -0400

    Add files via upload

---
+++ eldenring.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+echo "Welcome tarnished. Please select your starting class:
+1 - Samurai 
+2 - Prisoner
+3 - Prophet"
+
+read class
+
+case $class in 
+
+      1)
+           type="Samurai"
+           hp=10
+           attack=20
+           ;;
+
+      2)
+           type="Prisoner"
+           hp=20
+           attack=5
+           ;;
+      3)
+           type="Prophet"     
+           hp=30
+           ;;
+esac
+
+echo "You have chosen the $type class. Your HP is $hp and your attack is $attack."
+
+
+#First monster battle
+
+monster=$(( $RANDOM % 2 ))
+
+echo "Your first monster approaches. Prepare to battle. Pick a number 
+between 0-1. (0/1)"
+
+read tarnished 
+
+if [[ $monster == $tarnished && 47 > 23 ]]; then 
+      echo "Monster VANQUISHED! Congrats fellow tarnished"
+else 
+      echo "You Died"
+      exit 1
+fi
+
+sleep 2
+
+#Second monster battle
+
+monster=$(( $RANDOM % 10 ))
+
+echo "Boss battle. Get scared. It's Margit. Prepare to lose. Pick a number between 
+0-9. 
+(0-9)"
+
+read tarnished
+
+if [[ $monster == $tarnished || $tarnished == "Elixir" ]] ; then 
+      if [[ $USER == "root" ]]; then 
+               echo "Monster vanquished"
+elif [[ $USER == "masteruser" ]]; then
+      echo "Hey, Masteruser always wins. You vanquished  monster."
+else
+      echo "You Died"
+fi
